dd2a0a47cd12eeebad2b0b4fa33edd98fd50b706
caesiumtea <caesiumtea@gmail.com>
2023-03-09 08:22:37 -0500

    Write get_random

--- password.py
+++ password.py
@@ -1,19 +1,26 @@
-import random
+# Generate a random password of user-defined length, with randomization
+# seeded from an arbitrary string entered by the user.
 
 # Convert a string into a number for use as random seed
 def make_seed(s):
     seed = 1
-    for c in s:
+    # Loop through the string
+    for i in range(len(s)):
         # Convert each character to ASCI decimal number and multiply together
-        seed *= ord(c)
+        seed *= ord(s[i])
+        # Bitwise XOR each 2 adjacent characters and add to seed
+        if i % 2 == 1:
+            seed += ord(s[i]) ^ ord(s[i-1])
     return seed
 
 
-def get_random(lower_bound, upper_bound, keysmash, i):
-    return random.randint(lower_bound, upper_bound)
-    #num = seed*i
+def get_random(lower_bound, upper_bound, seed, i):
+    seed += i**3
+    num = seed % (upper_bound - lower_bound + 1) + lower_bound
+    return num
 
-# main function: 
+
+# main function: generate a random sequence of characters
 def generate_password():
     chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*-_+=<>?"
     chars_len = len(chars)
@@ -22,13 +29,19 @@ def generate_password():
     has_digit = False
     has_symbol = False
     loops = 0 # How many characters have been generated across all attempts
-              # Used for the second half of the random seed
+              # Used for the second half of the random seed, to make RNG result
+              # vary from one character to the next.
     
+    # Ask user how long a password to make, and keep asking if input is too 
+    # small or large 
     goal_len = 0
     while goal_len < 8 or goal_len > 64:
         goal_len = int(input("Enter a password length between 8 and 64:"))
+    
+    # To make a random seed, have the user press arbitrary letters.
+    # This is the first part of random seed, to make RNG results vary each time
+    # the program is run.
     keysmash = ""
-    # To make a random seed, have the user press arbitrary letters
     while len(keysmash) < 5:
         keysmash = input("Please enter a random keyboard smash of several letters:")
     seed = make_seed(keysmash)
@@ -45,7 +58,8 @@ def generate_password():
         # Add characters to the password until it reaches the requested length.
         # Picks a random number as an index in the string of possible characters.
         while len(password) < goal_len:
-            index = get_random(0, chars_len-1, keysmash, loops)
+            index = get_random(0, chars_len-1, seed, loops)
+            loops += 1
             char = chars[index]
             password += char
             # Check what kind of character was just added and update 
@@ -56,6 +70,5 @@ def generate_password():
             else: has_symbol = True
     # If the loop exits, then the password must have all necessary parts!
     return password
-
 
 print(generate_password())
